2482402e8cd616c6ba597f69db51597aa651a2fa
Ikram Hannechi <ikram.hannechi@esprit.tn>
2023-12-11 22:48:43 +0000

    s

---
+++ src/main/java/com/example/category/CategoryApplication.java
@@ -0,0 +1,28 @@
+package com.example.category;
+
+////import com.example.category.controller.CategoryController;
+//////import com.example.category.entity.Category;
+/////import com.example.category.repository.CategoryRepository;
+/////import org.springframework.beans.factory.annotation.Autowired;
+/////import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+///import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryApplication.class, args);
+    }
+///@Autowired
+///private CategoryRepository repository;
+///@Bean
+    ////ApplicationRunner init(){
+  /////////return (args) ->  {
+ /// repository.save(new Category(nom: "mariem", type: "jjjj", description:"olp"));
+ /////////// repository.findAll().forEach(System.out::println);};
+
+/////}
+
+}
