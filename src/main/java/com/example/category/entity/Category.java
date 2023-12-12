157d9ba9ffd861134ab5effb39102a51460c338a
Ikram Hannechi <ikram.hannechi@esprit.tn>
2023-12-12 15:03:55 +0000

    test

--- src/main/java/com/example/category/entity/Category.java
+++ src/main/java/com/example/category/entity/Category.java
@@ -20,7 +20,7 @@ public class Category implements Serializable {
     public String getNom() {
         return nom;
     }
-
+///
     public void setNom(String nom) {
         this.nom = nom;
     }
